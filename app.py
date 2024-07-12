4f8a79c82b21117aa845bd83d79eb7514dfa4a68
shungame65 <51528839+shungame65@users.noreply.github.com>
2024-07-12 13:24:29 +0900

    fixed double

--- app.py
+++ app.py
@@ -24,8 +24,8 @@ class users(Base):
 class locations(Base):
    __tablename__ = "locations" 
    user_id = Column(Integer, primary_key = True)
-   latitude = Column(Double)
-   longitude = Column(Double) 
+   latitude = Column(Float)
+   longitude = Column(Float) 
 
 
 class friends(Base): 
