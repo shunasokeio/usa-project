67d062ab6494e7eaf137aebbed32a52ed3c7efc0
shungame65 <51528839+shungame65@users.noreply.github.com>
2024-07-19 11:54:52 +0900

    import slightly tweaked

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 ## getting an error when trynna log in, it says the table is not created. might be something wrong with the link, the original link directly from heroku does not work. 
-
+print("started running")
 from flask import Flask, render_template, session, redirect, url_for, request
 from psycopg2 import Error
-from sqlalchemy import create_engine, Column, Integer, String, Float
+from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String, Date, Boolean
 try:
     from sqlalchemy.dialects.postgresql import ARRAY
     print("Import successful!")
